5849be649030841b2865f116c20840525d9e46b3
gitButter05 <jayinalbertjohnson@gmail.com>
2021-11-06 18:28:05 -0400

    Initial commit

---
+++ src/com/pokered/redmaprando/Main.java
@@ -0,0 +1,9 @@
+package com.pokered.redmaprando;
+
+public class Main {
+
+    public static void main(String[] args) {
+        if (args.length == 0)
+            System.out.println("Please input filepath of the decompiled ROM (pokered).");
+    }
+}
