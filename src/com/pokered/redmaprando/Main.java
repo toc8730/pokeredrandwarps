db3f87733a4fbc8c6816a30a557497968725bab2
gitButter05 <jayinalbertjohnson@gmail.com>
2021-11-06 19:52:38 -0400

    updated

--- src/com/pokered/redmaprando/Main.java
+++ src/com/pokered/redmaprando/Main.java
@@ -1,6 +1,13 @@
 package com.pokered.redmaprando;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -13,8 +20,25 @@ public class Main {
             return;
         }
         File mapObjects = new File(args[0] + "/data/maps/objects");
+        ArrayList<String> warpNames = new ArrayList<String>();
+        String ln;
         for (File mapObject : mapObjects.listFiles()) {
-
+            try {
+                Scanner reader = new Scanner(mapObject);
+                while (ln.substring(0, 13) != "\tdef_warps_to") // will loop until it finds the definition of the warp name
+                    ln = reader.nextLine();
+                warpNames.add(ln.substring(15)); // adds the "warps_to" token to the list
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+        Collections.shuffle(warpNames); // randomize
+        for (File mapObject : mapObjects.listFiles()) {
+            try {
+                FileWriter writer = new FileWriter(mapObject);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
