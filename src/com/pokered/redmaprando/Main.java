fc19fa5f5ff47a82afcf617be7bb7d6a37911cd7
gitButter05 <jayinalbertjohnson@gmail.com>
2021-11-06 18:56:26 -0400

    updated

--- src/com/pokered/redmaprando/Main.java
+++ src/com/pokered/redmaprando/Main.java
@@ -1,9 +1,20 @@
 package com.pokered.redmaprando;
 
+import java.io.File;
 public class Main {
 
     public static void main(String[] args) {
-        if (args.length == 0)
+        if (args.length == 0) {
             System.out.println("Please input filepath of the decompiled ROM (pokered).");
+            return;
+        }
+        if (args.length > 1) {
+            System.out.println("Invalid input(s). Please input filepath of the decompiled ROM (pokered).");
+            return;
+        }
+        File mapObjects = new File(args[0] + "/data/maps/objects");
+        for (File mapObject : mapObjects.listFiles()) {
+
+        }
     }
 }
