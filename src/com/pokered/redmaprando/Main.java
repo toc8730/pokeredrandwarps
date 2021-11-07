635c592b9b899d1abe96e78ff7d17ef79c5e99b1
gitButter05 <jayinalbertjohnson@gmail.com>
2021-11-06 21:09:28 -0400

    finished the writing to file part

--- src/com/pokered/redmaprando/Main.java
+++ src/com/pokered/redmaprando/Main.java
@@ -21,21 +21,30 @@ public class Main {
         }
         File mapObjects = new File(args[0] + "/data/maps/objects");
         ArrayList<String> warpNames = new ArrayList<String>();
+        ArrayList<Integer> offsets = new ArrayList<Integer>();
         String ln = new String();
         for (File mapObject : mapObjects.listFiles()) {
             try {
                 Scanner reader = new Scanner(mapObject);
-                while (ln.substring(0, 13) != "\tdef_warps_to") // will loop until it finds the definition of the warp name
+                int offset = 0;
+                while (ln.substring(0, 13) != "\tdef_warps_to") { // will loop until it finds the definition of the warp name
+                    offset += ln.length();
                     ln = reader.nextLine();
+                }
+                offset += 15; // should offset it exactly at the starting pos of the token
                 warpNames.add(ln.substring(15)); // adds the "warps_to" token to the list
+                offsets.add(offset);
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
             }
         }
         Collections.shuffle(warpNames); // randomize
+        int i = 0; // iterator
         for (File mapObject : mapObjects.listFiles()) {
             try {
                 FileWriter writer = new FileWriter(mapObject);
+                writer.write(warpNames.get(i), offsets.get(i), warpNames.get(i).length()); // overwrites the "warps_to" token in the current file
+                i++;
             } catch (IOException e) {
                 e.printStackTrace();
             }
