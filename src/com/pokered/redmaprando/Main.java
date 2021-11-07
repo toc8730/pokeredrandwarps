1e48065c300b300cfe109b9604d99e3800325feb
gitButter05 <jayinalbertjohnson@gmail.com>
2021-11-07 10:24:45 -0500

    finished the writing to file part

--- src/com/pokered/redmaprando/Main.java
+++ src/com/pokered/redmaprando/Main.java
@@ -24,19 +24,21 @@ public class Main {
             try {
                 Scanner reader = new Scanner(mapObject);
                 int offset = 0;
-                while (ln.substring(0, 13) != "\tdef_warps_to") { // will loop until it finds the definition of the warp name
+                while ((ln.length() > 13 ? ln.substring(0, 13) != "\tdef_warps_to" : true) && reader.hasNextLine()) { // will loop until it finds the definition of the warp name
                     offset += ln.length();
                     ln = reader.nextLine();
+                    offset++; // to account for newline
                 }
                 offset += 15; // should offset it exactly at the starting pos of the token
                 warpNames.add(ln.substring(15)); // adds the "warps_to" token to the list
                 offsets.add(offset);
+                reader.close();
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
             }
         }
         Collections.shuffle(warpNames); // randomize
-        int i = 0; // iterator
+        int i = 0; // iterator of warpNames and offsets
         for (File mapObject : mapObjects.listFiles()) {
             try {
                 FileWriter writer = new FileWriter(mapObject);
